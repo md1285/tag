0f43ea62bf5f0fa82916c7429dcaecd5046d0524
Michael Dwyer <michaeldwyer@Michaels-MacBook-Pro-3.local>
2019-02-22 13:28:21 -0800

    connect with local database

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/tag', {useNewUrlParser: true});
+
+// shortcut to mongoose.connection object
+var db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
